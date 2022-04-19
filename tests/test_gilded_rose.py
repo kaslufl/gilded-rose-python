e3adf2df465c073c1a732c8dae4a0ac983e26d85
kaslufl <leallucasfonseca@gmail.com>
2022-04-18 21:42:03 -0300

    test: add normal item tests

--- tests/test_gilded_rose.py
+++ tests/test_gilded_rose.py
@@ -5,11 +5,17 @@ from gilded_rose.gilded_rose import Item, GildedRose
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_item_quality_cannot_be_negative(self):
+        items = [Item("Elixir of the Mongoose", 3, 0)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual(0, items[0].quality)
+
+    def test_item_quality_when_sell_in_expires(self):
+        items = [Item("Elixir of the Mongoose", 0, 20)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(18, items[0].quality)
 
 
 if __name__ == '__main__':
