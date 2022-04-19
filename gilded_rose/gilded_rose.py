0ec8effb15df86405d726328be578e4f1514bc85
kaslufl <leallucasfonseca@gmail.com>
2022-04-19 20:10:29 -0300

    refact: finish project

--- gilded_rose/gilded_rose.py
+++ gilded_rose/gilded_rose.py
@@ -18,8 +18,18 @@ class GildedRose(object):
             self.update_item(item)
 
     def update_item(self, item):
+        quality_decrese_rate = DECREASE_RATE
+        if self.is_item_expired(item):
+            quality_decrese_rate = quality_decrese_rate * 2
+
+        if self.is_conjured(item):
+            quality_decrese_rate = quality_decrese_rate * 2
+
+        if not self.is_sulfuras(item):
+            self.adjust_sell_in(DECREASE_RATE, item)
+
         if self.is_decrementable(item):
-            self.adjust_quality(DECREASE_RATE, item)
+            self.adjust_quality(quality_decrese_rate, item)
 
         if self.is_aged_brie(item):
             self.adjust_quality(INCREASE_RATE, item)
@@ -27,17 +37,8 @@ class GildedRose(object):
         if self.is_backstage_pass(item):
             self.adjust_backstage_quality(item)
 
-        if not self.is_sulfuras(item):
-            self.adjust_sell_in(DECREASE_RATE, item)
-
-        if self.is_item_expirable(item):
-            if self.is_decrementable(item):
-                self.adjust_quality(DECREASE_RATE, item)
-            if self.is_backstage_pass(item):
-                item.quality -= item.quality
-
-    def is_item_expirable(self, item):
-        return item.sell_in < EXPIRED_VALUE
+    def is_item_expired(self, item):
+        return item.sell_in <= EXPIRED_VALUE
 
     def is_decrementable(self, item):
         return item.name != ItemEnum.AGED_BRIE.value and item.name != ItemEnum.BACKSTAGE_PASS.value \
@@ -65,6 +66,11 @@ class GildedRose(object):
             self.adjust_quality(INCREASE_RATE, item)
         if item.sell_in < 6:
             self.adjust_quality(INCREASE_RATE, item)
+        if self.is_item_expired(item):
+            item.quality -= item.quality
 
     def is_backstage_pass(self, item):
         return item.name == ItemEnum.BACKSTAGE_PASS.value
+
+    def is_conjured(self, item):
+        return item.name == ItemEnum.CONJURED.value
